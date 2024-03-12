4efd0c19c2d9dc0819d61ce9a9ac0cc5e995ce20
vaibhav-vm-vidhya <vaibhavvakil1234@gmail.com>
2024-03-12 20:52:01 +0530

    cart apis created

--- routes/orders.js
+++ routes/orders.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Order = require('../models/Orders'); // Assuming your module is named 'orderModel'
+const User = require("../models/Users");
+
 
 // Route to place a new order
 router.post('/place-order', async (req, res) => {
@@ -42,6 +44,39 @@ router.get('/', async (req, res) => {
   try {
     const orders = await Order.find();
     res.json(orders);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+router.post('/:userId/add-to-basket', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const products = req.body;
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    // console.log(products.products)
+    user.basket = products.products
+    await user.save();
+
+    res.json(user.basket);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.get('/:userId/basket', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const user = await User.findById(userId);
+    
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(user.basket);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
